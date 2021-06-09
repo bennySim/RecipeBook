366307a0ab1fd7323134dba40f13972ad5c783f4
Simona Bennárová <s.bennarova@gmail.com>
2021-06-09 12:41:26 +0200

    Added message "Recipes with <selected ingredient>" to page

--- WebApplication2/Pages/RecipePages/SearchAccordingIngredient.cshtml.cs
+++ WebApplication2/Pages/RecipePages/SearchAccordingIngredient.cshtml.cs
@@ -18,10 +18,8 @@ namespace WebApplication2.Pages.RecipePages
         }
 
         [BindProperty]
-        public List<RecipeIngredient> IngredientsInRecipe { get; set; } =
+        public List<IngredientInRecipe> IngredientsInRecipe { get; set; } =
             new() {new()};
-
-        public List<RecipeIngredient> Ingredients { get; set; }
 
         public List<Ingredient> Ingredient { get; set; }
         public List<Recipe> Recipe { get; set; } = new();
@@ -33,15 +31,22 @@ namespace WebApplication2.Pages.RecipePages
 
         public async Task OnPost()
         {
-            var recipeIngredients = await _context.Set<RecipeIngredient>().ToListAsync();
             Ingredient = await _context.Set<Ingredient>().ToListAsync();
+            WriteMessage();
+            await FindRecipesWithIngredients();
+        }
+
+        private async Task FindRecipesWithIngredients()
+        {
+            var recipeIngredients = await _context.Set<RecipeIngredient>().ToListAsync();
             var recipes = await _context.Set<Recipe>().ToListAsync();
+
             Recipe = IngredientsInRecipe.Where(r => r.Count > 0)
                 .Join(recipeIngredients, ri => ri.IngredientId, ri => ri.IngredientId, (ri1, ri2) =>
                     new {Recipe = ri2, ri1.Count})
                 .Where(r => r.Recipe.Count <= r.Count)
                 .GroupBy(r => r.Recipe.RecipeId)
-                .Join(recipes, rg => rg.Key, r => r.Id, (rg, r) => 
+                .Join(recipes, rg => rg.Key, r => r.Id, (rg, r) =>
                     new
                     {
                         Recipe = r,
@@ -51,5 +56,25 @@ namespace WebApplication2.Pages.RecipePages
                 .Select(g => g.Recipe)
                 .ToList();
         }
+
+        private void WriteMessage()
+        {
+            ViewData["Message"] = "Recipes with " + IngredientsInRecipe
+                .Join(Ingredient, i => i.IngredientId, i => i.Id, (ir, i) =>
+                    new
+                    {
+                        i.Name,
+                        i.Unit,
+                        ir.Count
+                    })
+                .Select(i => i.Count + " " + i.Unit + " of " + i.Name)
+                .Aggregate((r1, r2) => r1 + ", " + r2);
+        }
+    }
+
+    public class IngredientInRecipe
+    {
+        public int IngredientId { get; set; }
+        public uint Count { get; set; }
     }
 }
