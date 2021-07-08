92175d37ba778edf4cd543e4fa994376518b09e9
Simona Bennárová <s.bennarova@gmail.com>
2021-07-08 16:03:25 +0200

    Minor refactoring in Startup.cs

--- RecipeBook/Startup.cs
+++ RecipeBook/Startup.cs
@@ -10,25 +10,27 @@ namespace WebApplication2
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
             services.AddServerSideBlazor();
-            var connectionString = Configuration.GetConnectionString("RecipeDB").Replace("{AppDir}", Directory.GetCurrentDirectory());
+
+            var connectionString = Configuration
+                .GetConnectionString("RecipeDB")
+                .Replace("{AppDir}", Directory.GetCurrentDirectory());
             services.AddDbContext<RecipesContext>(
                 options => options
                     .UseSqlite(connectionString));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,17 +40,13 @@ namespace WebApplication2
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
         }
     }
